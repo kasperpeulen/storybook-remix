8e677313ab78ab2e10eb61125837d5c02cd3411d
Kasper Peulen <kasperpeulen@gmail.com>
2022-12-26 20:24:57 +0100

    Add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import { StorybookConfig } from "@storybook/react-vite";
+
+export default {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+} satisfies StorybookConfig;
---
+++ .storybook/preview.ts
@@ -0,0 +1,11 @@
+import { Parameters } from "@storybook/react";
+
+export const parameters: Parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
