f611ae0ea1b973c2679f6ce52fda001574b49630
Kasper Peulen <kasperpeulen@gmail.com>
2022-12-27 13:07:30 +0100

    Starting point of tutorial

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,14 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+import { LiveReload } from "@remix-run/react";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <Meta />
-        <Links />
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        Hello world
         <LiveReload />
       </body>
     </html>
