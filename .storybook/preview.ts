d12408a06646aa138d865fe9a8007ba968c0e469
Kasper Peulen <kasperpeulen@gmail.com>
2022-12-28 14:24:16 +0100

    Debug emoji

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,14 @@
 import { Parameters } from "@storybook/react";
 
+import isChromatic from "chromatic/isChromatic";
+
+const fontLoader = async () => ({
+  // or
+  fonts: await document.fonts.ready,
+});
+
+export const loaders = isChromatic() && document.fonts ? [fontLoader] : [];
+
 export const parameters: Parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
