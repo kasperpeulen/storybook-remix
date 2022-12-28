1f62cf64ff04aff3ab10efb1701fea4e97db1a05
Kasper Peulen <kasperpeulen@gmail.com>
2022-12-28 18:26:31 +0100

    Add @storybook/addon-a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ export default {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
