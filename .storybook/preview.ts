bffae158b1c77849dfbd5bbe87fd388e039bf203
Kasper Peulen <kasperpeulen@gmail.com>
2022-12-28 13:59:06 +0100

    Debug emojis

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,19 @@
 import { Parameters } from "@storybook/react";
 
+import isChromatic from "chromatic/isChromatic";
+
+// Use the document.fonts API to check if fonts have loaded
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/fonts API to
+const fontLoader = async () => ({
+  fonts: await document.fonts.ready,
+});
+
+/* 👇 It's configured as a global loader
+ * See https://storybook.js.org/docs/react/writing-stories/loaders
+ * to learn more about loaders
+ */
+export const loaders = isChromatic() && document.fonts ? [fontLoader] : [];
+
 export const parameters: Parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
