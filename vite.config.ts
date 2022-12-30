a4575f584a1b6c43997587ac4d5af0503bf2b3d5
Kasper Peulen <kasperpeulen@gmail.com>
2022-12-30 16:39:09 +0100

    Setup coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: [{ find: "~", replacement: path.resolve(__dirname, "./app") }],
   },
-  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    istanbul({
+      checkProd: false,
+      forceBuildInstrument: true,
+      include: ["app/routes/*", "app/utils/request*", "app/utils/session*"],
+      exclude: ["node_modules", "test/", "*.stories*"],
+      extension: [".ts", ".tsx"],
+    }),
+  ],
 });
