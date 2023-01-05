5e15daf55f71d394092cca1598fb6ae11a9f1a05
Kasper Peulen <kasperpeulen@gmail.com>
2023-01-05 11:30:40 +0100

    Preload all css and load all root css with loader

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,9 @@
-import { Parameters } from "@storybook/react";
-import { testAppDefaultProps } from "~/test/TestApp";
+import { Loader, Parameters } from "@storybook/react";
+import { LinkDescriptor } from "@remix-run/node";
+import { links } from "~/root";
+import { links as indexLinks } from "~/routes";
+import { links as jokesLinks } from "~/routes/jokes";
+import { links as loginLinks } from "~/routes/login";
 
 export const parameters: Parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,3 +17,36 @@ export const parameters: Parameters = {
     autoplay: true,
   },
 };
+
+export const loaders: Loader[] = [
+  () => linksLoader(links()),
+  () =>
+    linksLoader(
+      [...indexLinks(), ...jokesLinks(), ...loginLinks()].flatMap((link) =>
+        !("rel" in link && link.rel === "stylesheet") ? [] : [{ href: link.href ?? "", rel: "preload", as: "style" }]
+      )
+    ),
+];
+
+export async function linksLoader(links: LinkDescriptor[]) {
+  const loadedLinks: LinkDescriptor[] = [];
+
+  const head = document.head;
+  links.forEach((link) => {
+    const element = document.createElement("link");
+    // @ts-ignore
+    for (const [key, value] of Object.entries(link)) element[key] = value;
+    element.onload = () => loadedLinks.push(link);
+    head.appendChild(element);
+  });
+  await waitFor(() => loadedLinks.length === links.length);
+  return {};
+}
+
+export default function waitFor(condition: () => boolean, times = 100): Promise<null> {
+  if (!condition() && times > 0) {
+    return new Promise(requestAnimationFrame).then(() => waitFor(condition, --times));
+  } else {
+    return Promise.resolve(null);
+  }
+}
